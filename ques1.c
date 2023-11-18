4e49f4e077f2a6ba72cb1e9e980e367f35e44f48
kushalmeghwal <kushalmeghwal4@gmail.com>
2023-11-18 11:28:25 +0530

    addition

---
+++ ques1.c
@@ -0,0 +1,20 @@
+//addition
+#include<stdio.h>
+int main()
+{
+    int a,b,sum,sub,mul,div;
+    int c;
+    printf("enter two numbers\n");
+    scanf("%d %d",&a,&b);
+    sum=a+b;
+    sub=a-b;
+    mul=a*b;
+    div=a/b;
+    printf("enter choice\n ");
+    scanf("%d",&c);
+    switch(c) {
+    case 1: printf("sum = %d\n",sum);
+             break;
+    return 0;
+    }
+}
